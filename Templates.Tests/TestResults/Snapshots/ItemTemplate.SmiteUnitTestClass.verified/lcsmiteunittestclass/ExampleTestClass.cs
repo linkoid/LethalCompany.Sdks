ffbe90f3517e76da5475a075682d92566028c7b5
linkoid <36754150+linkoid@users.noreply.github.com>
2024-09-18 22:39:25 -0400

    Include template snapshots in source control

---
+++ Templates.Tests/TestResults/Snapshots/ItemTemplate.SmiteUnitTestClass.verified/lcsmiteunittestclass/ExampleTestClass.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using BepInEx.Logging;
+using SmiteUnit.Framework;
+using SmiteUnit.Unity;
+using UnityEngine;
+
+namespace MyPlugin.Tests
+{
+    // The SmiteProcessAttribute tells the test adapter which program to start
+    [SmiteProcess("%GAME_EXE_PATH%", "-logfile -")]
+    public class ExampleTestClass : MonoBehaviour
+    {
+        private static ExampleTestClass Instance = null!;
+        private static ManualLogSource Logger = null!;
+
+        // The SmiteSetUpAttribute marks methods that should run before any test methods
+        [SmiteSetUp]
+        public static void SetUp()
+        {
+            Instance = new GameObject($"{nameof(ExampleTestClass)}").AddComponent<ExampleTestClass>();
+            GameObject.DontDestroyOnLoad(Instance.gameObject);
+            Instance.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+            Logger = BepInEx.Logging.Logger.CreateLogSource(typeof(ExampleTestClass).Assembly.GetName().Name);
+        }
+
+        // The SmiteTestAttribute marks methods that can be run as tests
+        [SmiteTest]
+        public static void HelloWorldTest()
+        {
+            Logger.LogInfo("Hello World!");
+        }
+
+        // An async test can be run with a coroutine using the StartCoroutineTask extension method
+        [SmiteTest]
+        public static async Task CoroutineTest()
+        {
+            await Instance.StartCoroutineTask(MyLocalCoroutine());
+            IEnumerator MyLocalCoroutine()
+            {
+                // Async test logic goes here
+                yield return new WaitForSeconds(1);
+                Logger.LogInfo("Waited for 1 second.");
+            }
+        }
+    }
+}
---
+++ Templates.Tests/TestResults/Snapshots/ProjectTemplate.SmiteUnitPlugin.verified/lcsmiteunitplugin/Example.Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace Example.Plugin
+{
+    [BepInPlugin("Example.Plugin", "Example_Plugin", "1.0")]
+    public class Example_Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+
+#if DEBUG
+            // Create an injection helper using the name of the test assembly
+            var injectionHelper = UnitySmiteInjectionHelper.Create("Example.Plugin.Tests");
+
+            // Call Start() after creation to start tests immediately
+            injectionHelper.Start();
+
+            // Or don't call Start() and tests will automatically start on the next frame.
+#endif
+        }
+    }
+}
